a06fa86285a5691262a9965b60c91d45a2b4ab43
Jeanne0410 <jeanne.leroux@etudiant.univ-rennes2.fr>
2024-10-07 14:37:59 +0200

    changement

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-import requests
+from urllib.request import Request
 
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 url = "https://www.bandsintown.com/choose-dates/fetch-next/upcomingEvents?date=&page=2&longitude=-0.12574&latitude=51.50853&genre_query=all-genres"
 
 req = Request(url,headers={'User-Agent':user_agent})

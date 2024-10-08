3b770a067bf40aa7e06ea254f819360207f49fae
titouancb <titouan.cabon@gmail.com>
2024-10-08 11:02:32 +0200

    transformation pour tout les dataframes

--- transformation.py
+++ transformation.py
@@ -1,4 +1,11 @@
 import json
+import pandas as pd
+from pandas import json_normalize
 
-fichier = json.load("json_data/data_page_1.json")
-print(fichier)
+# Charger le fichier JSON
+with open("json_data/data_page_1.json", "r", encoding="utf-8") as f:
+    fichier = json.load(f)
+
+#print(fichier)
+df = json_normalize(fichier["events"])  # Results contain the required data
+print(df)
